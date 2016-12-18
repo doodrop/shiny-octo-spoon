f3b10783f1d94dfe10dbc866bea8fb4ef5f4b5c8
justrdk <osman.sist@gmail.com>
2016-12-18 15:27:10 -0600

    initial setup

---
+++ dooDrop/app/components/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Hello = () => (
+	<View>
+		<Text>Hello DooDrop</Text>
+	</View>
+);
+
+export default Hello;
---
+++ dooDrop/index.android.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { AppRegistry } from 'react-native';
+import { Actions, Scene, Router, ActionConst } from 'react-native-router-flux';
+import Hello from './app/components/Hello';
+// import reducers from './app/reducers/index';
+
+const loggerMiddleware = createLogger();
+const store = createStore(() => true, applyMiddleware(
+  thunkMiddleware,
+  loggerMiddleware
+));
+
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="hello" component={Hello} hideNavBar initial />
+  </Scene>
+);
+
+
+const dooDrop = () => (
+    <Provider store={store}>
+      <Router scenes={scenes} />
+    </Provider>
+);
+
+AppRegistry.registerComponent('dooDrop', () => dooDrop);
